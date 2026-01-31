31c316d16efb4b7681a8d422a3e2458a2de9da79
AroaMG Mac <aroaborealus@gmail.com>
2026-01-31 13:39:35 +0100

    Initial Commit

---
+++ DnDCompanion/DnDCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnDCompanionApp.swift
+//  DnDCompanion
+//
+//  Created by Aroa Miguel Garcia on 31/1/26.
+//
+
+import SwiftUI
+
+@main
+struct DnDCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
